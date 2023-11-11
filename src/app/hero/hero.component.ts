ac28cbccaa200fe6268189f8d9ba22c7a5af4caf
Shahil Kumar <96991517+Shahil9728@users.noreply.github.com>
2023-11-11 13:31:45 +0530

    adding more functionalites

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+  constructor(private renderer: Renderer2) { }
 
+  isReadMoreVisible = false;
+
+  readmore() {
+    const para = document.querySelector('#readpara');
+
+    if (this.isReadMoreVisible) {
+      this.renderer.setStyle(para, 'display', 'none');
+      this.renderer.setStyle(para, 'height', '0');
+    } else {
+      this.renderer.setStyle(para, 'display', 'block');
+      this.renderer.setStyle(para, 'height', 'auto');
+    }
+    this.isReadMoreVisible = !this.isReadMoreVisible;
+  }
+  isReadMoreVisible1 = false;
+
+  readmore1() {
+    const para = document.querySelector('#readpara1');
+    if (this.isReadMoreVisible1) {
+      this.renderer.setStyle(para, 'display', 'none');
+      this.renderer.setStyle(para, 'height', '0');
+    } else {
+      this.renderer.setStyle(para, 'display', 'block');
+      this.renderer.setStyle(para, 'height', 'auto');
+    }
+    this.isReadMoreVisible1 = !this.isReadMoreVisible1;
+  }
 }
